81ea0a35e09b8bced438120eb7b0d02134b8c6dd
JohnMcCarthy <john@cloudmine.me>
2013-12-23 21:24:29 -0500

    Initial create; login screen

---
+++ src/com/mccarthy/activities/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.mccarthy.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.mccarthy.R;
+
+/**
+ * <br>
+ * Copyright CloudMine LLC. All rights reserved<br>
+ * See LICENSE file included with SDK for details.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
